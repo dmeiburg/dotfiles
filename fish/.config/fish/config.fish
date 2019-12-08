d0848e02f1c73724919b00877d21face7f592b3b
Daniel Meiburg <daniel@bluethmann.de>
2019-12-08 01:55:03 +0100

    Move variables and abbr to config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,4 +5,24 @@ if status is-login
     end
 end
 
+# Shortcuts
+abbr g git
+abbr gc git -c 
+abbr v nvim
+abbr vim nvim
+abbr port ss -tulanp
+abbr xi xbps-install -Sy
+abbr xq	xbps-query
+abbr xu xbps-install -Su
+
+# Variables
+set -x TERMINAL st
+set -x VISUAL nvim
+set -x READER zathura
+set -x NNN_USE_EDITOR 1
+set -x EDITOR nvim
+set -x BROWSER firefox
+set -x fish_key_bindings fish_vi_key_bindings
+set -x fish_greeting ""
+set -x FZF_DEFAULT_COMMAND find .
 source ~/.ssh/ssh-agent-env
